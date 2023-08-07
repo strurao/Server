e49ab7dee37a5b4392a35ab22fbd9e079f00850f
strurao <strurao@gmail.com>
2023-08-07 20:14:17 +0900

    test: lock

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -21,7 +21,7 @@ Data    O     공유하므로 복잡한 문제가 생길 수 있다    전역/st
 // 만약 3개의 스레드가 접근하려면, 누가 먼저 접근할지 알 수 없다. 가장 빠른 녀석이 자물쇠를 풀고 (임계영역) 화장실을 사용하는 격이다.
 // 1,2,3, 중에서 1번이 먼저 갔다가 또다시 1번이 또 들어갈 수도 있다. 아닐 수도 있고. 이것이 경합이다.
 // RW Lock같은 특이 케이스를 제외하고 기본적으로 Lock은 한번에 하나이다. 그것이 동기화이다
-// ex) airplane toilet
+// Ex) Airplane's Toilet
 mutex m;
 
 vector<int> v;
@@ -30,6 +30,7 @@ vector<int> v;
 // RAII (Resource Acquisition Is Initialization)
 // 일종의 Wrapper Class 이다
 // std::lock_guard
+/*
 template<typename T>
 class LockGuard
 {
@@ -47,7 +48,60 @@ public:
 private:
 	T& _mutex;
 };
+*/
 
+// 우리만의 락
+class Lock
+{
+public:
+	void lock()
+	{
+		// 아래의 코드는 상호배타성에 어긋난다. 스레드가 틈새로 들어올 수 있다
+		/*
+		while (_flag)
+		{
+			// Do Nothing
+		}
+		_flag = true;
+		*/
+
+		// 그래서 사용하는 CAS (Compare-And-Swap)
+		bool expected = false;
+		bool desired = true;
+
+		// Spin Lock: spin이 계속 뺑뺑이 돌듯 쉬지않고 노크를 한다는 것 (존버멘탈)
+		// 장점:
+		// 단점: CPU cost 가 크다. 기내화장실 대기 중인데 앞사람이 술취 후 잠들었다면?
+		while (_flag.compare_exchange_strong(expected, desired) == false)
+		{
+			expected = false;
+		}
+		
+		// 이것의 수도코드
+		/*
+		if (_flag == expected)
+		{
+			_flag = desired;
+			return true;
+		}
+		else
+		{
+			expected = _flag;
+			return false;
+		}
+		*/
+	}
+
+	void unlock()
+	{
+		_flag = false;
+	}
+
+private:
+	atomic<bool> _flag = false;
+};
+
+Lock m2;
 
 void Push()
 {
@@ -57,10 +111,13 @@ void Push()
 		// v.push_back(i);
 		// m.unlock(); // 자물쇠처럼 잠그고 풀기. 이걸 주석으로 하면 나머지 애들은 영영 대기이다.
 		//LockGuard<mutex> lockGuard(m);
-		std::lock_guard<mutex> lockGuard(m); // 이렇게 모두 다 lock을 거는 것은 정답이 아니다
+		
+		//std::lock_guard<mutex> lockGuard(m); // 이렇게 모두 다 lock을 거는 것은 정답이 아니다
 
 		//std::unique_lock<mutex> uniqueLock(m, std::defer_lock); // lock_guard 와 거의 동일하나, 개발자 수동 잠금
 		//uniqueLock.lock();
+
+		std::lock_guard<Lock> lockGuard(m2);
 
 		v.push_back(i);
 
@@ -71,23 +128,6 @@ void Push()
 	}
 }
 
-
-void SetCursorPosition(int x, int y)
-{
-	HANDLE output = ::GetStdHandle(STD_OUTPUT_HANDLE);
-	COORD pos = { (SHORT) x, (SHORT)y };
-	::SetConsoleCursorPosition(output, pos);
-}
-
-void SetCursorOnOff(bool visible)
-{
-	HANDLE output = ::GetStdHandle(STD_OUTPUT_HANDLE);
-	CONSOLE_CURSOR_INFO cursorInfo;
-	::GetConsoleCursorInfo(output, &cursorInfo);
-	cursorInfo.bVisible = visible;
-	::SetConsoleCursorInfo(output, &cursorInfo);
-}
-
 void HelloThread(int i)
 {
 	while(true)
@@ -96,10 +136,9 @@ void HelloThread(int i)
 	}
 }
 
-
-
 int main()
 {
+	/*
 	vector<thread> threads;
 
 	// 직원 배치하기
@@ -118,7 +157,16 @@ int main()
 		int a = t.hardware_concurrency(); // 병렬처리 가능한 CPU 코어 개수. 내 PC는 16개이다
 		t.join(); // 끝날 때까지 대기한다
 	}
-		
-	// SetCursorOnOff(false);
+	*/
+
+	v.reserve(100000);
+
+	thread t1(Push);
+	thread t2(Push);
+
+	t1.join();
+	t2.join();
+
+	cout << v.size() << endl;
 }
 
