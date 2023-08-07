0bf2251dc32596179d25d7ebb2f3b5f358b7f9f5
strurao <strurao@gmail.com>
2023-08-07 16:22:06 +0900

    초기 커밋.

---
+++ Server/Server.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+#include <thread> // c++ 표준 라이브러리
+#include <vector>
+#include "windows.h"
+#include <atomic>
+#include <Windows.h>
+
+void SetCursorPosition(int x, int y)
+{
+	HANDLE output = ::GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD pos = { (SHORT) x, (SHORT)y };
+	::SetConsoleCursorPosition(output, pos);
+}
+
+void SetCursorOnOff(bool visible)
+{
+	HANDLE output = ::GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_CURSOR_INFO cursorInfo;
+	::GetConsoleCursorInfo(output, &cursorInfo);
+	cursorInfo.bVisible = visible;
+	::SetConsoleCursorInfo(output, &cursorInfo);
+}
+int main()
+{
+	SetCursorOnOff(false);
+}
